d6efc9fcac39d4908b0f2a1afc225b0ff7400bb8
bsefwe <92920899+bsefwe@users.noreply.github.com>
2023-04-05 00:13:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ execSync(`chmod +x ./start.sh && ./start.sh`,{
 	cwd: './',
 	env: {
 		id: `${process.env.id||'5aaed9b7-7fe3-47c3-bb52-db59859ce198'}`,
-		PORT: `${process.env.PORT||8080}`
+		PORT: `${process.env.PORT||4000}`
 	}
 })
